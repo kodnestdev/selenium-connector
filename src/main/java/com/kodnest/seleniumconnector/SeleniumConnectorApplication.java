604b4e776f30f2987267ab8031ccd48c968846ba
Administrator <admin@example.com>
2023-06-17 03:22:53 +0530

    INIT-Commit

---
+++ src/main/java/com/kodnest/seleniumconnector/SeleniumConnectorApplication.java
@@ -0,0 +1,13 @@
+package com.kodnest.seleniumconnector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeleniumConnectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeleniumConnectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kodnest/seleniumconnector/SeleniumConnectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodnest.seleniumconnector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeleniumConnectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
